e5264281beb1d0f33e77698721da8e9d777953c5
Rohan <rohan8dave@gmail.com>
2025-10-14 15:22:44 -0400

    rm unused console2 import

--- src/libraries/QuoteReverter.sol
+++ src/libraries/QuoteReverter.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.30;
 
-import {console2} from "forge-std/console2.sol";
-
 /// @title QuoteReverter
 /// @notice Library for parsing data from revert reasons so that accurate data about transaction outcomes can be
 /// obtained without changing state
