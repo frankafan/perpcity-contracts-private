9f1a783c8152203ed5bb2aea38f857ca6b029e02
frankafan <frank.a.fan@outlook.com>
2025-10-31 05:52:47 -0400

    Change test constants.

--- halmos/PerpManagerTest.t.sol
+++ halmos/PerpManagerTest.t.sol
@@ -121,7 +121,7 @@ contract PerpManagerHalmosTest is SymTest, Test {
         vm.assume(caller != address(perpManager));
         vm.assume(caller != vault);
 
-        _callPerpManagerNTimes(selector, caller, perpId1, 1);
+        _callPerpManagerNTimes(selector, caller, perpId1, 2);
 
         uint256 vaultBalanceAfter = usdcMock.balanceOf(vault);
         uint128 insuranceAfter = perpManager.getInsurance(perpId1);
